c0930b637abddd30878c28e24ed34337ee8cfcb0
nguerra040 <52302560+nguerra040@users.noreply.github.com>
2022-10-26 03:36:09 -0500

    Add files via upload

--- timesteppers.py
+++ timesteppers.py
@@ -122,7 +122,7 @@ class AdamsBashforth(Timestepper):
         b = np.array([1, 4, 1])/6
         for i in range(1,steps):
             ts = Multistage(u_archives[:,i-1], self.func, stages, a, b)
-            ts.step(self.dt/100)
+            ts.step(self.dt/10)
             u_archives[:,i] = np.copy(ts.u)
         self.u_archives = np.copy(u_archives)
         self.u = np.copy(ts.u)
@@ -226,3 +226,176 @@ class BackwardDifferentiationFormula(Timestepper):
             self.u_archives[:, 1:] = np.copy(self.u_archives[:, :-1])
             self.u_archives[:, 0] = np.copy(new_u)
             return new_u
+
+################################## IMEX ####################################
+class StateVector:
+    
+    def __init__(self, variables):
+        var0 = variables[0]
+        self.N = len(var0)
+        size = self.N*len(variables)
+        self.data = np.zeros(size)
+        self.variables = variables
+        self.gather()
+    def gather(self):
+        for i, var in enumerate(self.variables):
+            np.copyto(self.data[i*self.N:(i+1)*self.N], var)
+    def scatter(self):
+        for i, var in enumerate(self.variables):
+            np.copyto(var, self.data[i*self.N:(i+1)*self.N])
+            
+class IMEXTimestepper:
+    def __init__(self, eq_set):
+        self.t = 0
+        self.iter = 0
+        self.X = eq_set.X
+        self.M = eq_set.M
+        self.L = eq_set.L
+        self.F = eq_set.F
+        self.dt = None
+    def evolve(self, dt, time):
+        while self.t < time - 1e-8:
+            self.step(dt)
+    def step(self, dt):
+        self.X.data = self._step(dt)
+        self.X.scatter()
+        self.t += dt
+        self.iter += 1
+        
+class Euler(IMEXTimestepper):
+    def _step(self, dt):
+        if dt != self.dt:
+            LHS = self.M + dt*self.L
+            self.LU = spla.splu(LHS.tocsc(), permc_spec='NATURAL')
+        self.dt = dt
+        
+        RHS = self.M @ self.X.data + dt*self.F(self.X)
+        return self.LU.solve(RHS)
+    
+class CNAB(IMEXTimestepper):
+    def _step(self, dt):
+        if self.iter == 0:
+            # Euler
+            LHS = self.M + dt*self.L
+            LU = spla.splu(LHS.tocsc(), permc_spec='NATURAL')
+            self.FX = self.F(self.X)
+            RHS = self.M @ self.X.data + dt*self.FX
+            self.FX_old = self.FX
+            return LU.solve(RHS)
+        else:
+            if dt != self.dt:
+                LHS = self.M + dt/2*self.L
+                self.LU = spla.splu(LHS.tocsc(), permc_spec='NATURAL')
+            self.dt = dt
+            self.FX = self.F(self.X)
+            RHS = self.M @ self.X.data - 0.5*dt*self.L @ self.X.data + 3/2*dt*self.FX - 1/2*dt*self.FX_old
+            self.FX_old = self.FX
+            return self.LU.solve(RHS)
+        
+class BDFExtrapolate(IMEXTimestepper):
+    def __init__(self, eq_set, steps):
+        super().__init__(eq_set)
+        self.steps = steps
+        self.current_total_steps = 1
+        self.dt_array = np.array([])
+        # oldest X is at the top so X_n-1 (current) is at bottom
+        self.X_archives = [self.X]
+        self.FX_archives = []
+        
+    
+    def _step(self, dt):
+        self.X_archives.append(self.X.data)
+        self.FX_archives.append(self.F(self.X))
+        if dt != self.dt:
+            self.dt = np.copy(dt)
+        if self.current_total_steps < self.steps:
+            # solve first iterations with lower order method
+            # Append most recent dt to top
+            self.dt_array = np.append(self.dt, self.dt_array)
+            
+            # compute ai coefficients
+            A = np.zeros((self.current_total_steps+1, self.current_total_steps+1))
+            A[0,:] = 1
+            for i in range(1, self.current_total_steps+1):
+                for j in range(1, self.current_total_steps+1):
+                    A[i,j] = ((-j*dt)**i)/math.factorial(i)
+            b = np.zeros(self.current_total_steps+1)
+            b[1] = 1
+            lu, piv = lu_factor(A)
+            # a is the coefficients with a0 at the top
+            a = lu_solve((lu, piv), b)
+            
+            # compute bi coefficients
+            A = np.zeros((self.current_total_steps, self.current_total_steps))
+            A[0,:] = 1
+            for i in range(1, self.current_total_steps):
+                for j in range(self.current_total_steps):
+                    A[i,j] = ((-(j+1)*dt)**i)/math.factorial(i)
+            b = np.zeros(self.current_total_steps)
+            b[0] = 1
+            lu, piv = lu_factor(A)
+            # bb is the coefficients with b1 at the top
+            bb = lu_solve((lu, piv), b)
+            
+            LHS = self.M*a[0] + self.L
+            
+            First_RHS_Term = bb[0]*self.FX_archives[-1]
+            for i in range(1, self.current_total_steps):
+                First_RHS_Term += bb[i]*self.FX_archives[-(i+1)]
+                
+            Second_RHS_Term = a[1]*self.X_archives[-1]
+            for i in range(2, self.current_total_steps+1):
+                Second_RHS_Term += a[i]*self.X_archives[-i]
+                
+            RHS = First_RHS_Term - (self.M @ Second_RHS_Term)
+            
+            self.LU = spla.splu(LHS.tocsc(), permc_spec='NATURAL')
+            new_X = self.LU.solve(RHS)
+            self.current_total_steps += 1
+            return new_X
+        
+        else:
+            # Append most recent dt to top
+            self.dt_array = np.append(self.dt, self.dt_array)
+            
+            # compute ai coefficients
+            A = np.zeros((self.steps+1, self.steps+1))
+            A[0,:] = 1
+            for i in range(1, self.steps+1):
+                for j in range(1, self.steps+1):
+                    A[i,j] = ((-j*dt)**i)/math.factorial(i)
+            b = np.zeros(self.steps+1)
+            b[1] = 1
+            lu, piv = lu_factor(A)
+            # a is the coefficients with a0 at the top
+            a = lu_solve((lu, piv), b)
+            
+            # compute bi coefficients
+            A = np.zeros((self.steps, self.steps))
+            A[0,:] = 1
+            for i in range(1, self.steps):
+                for j in range(self.steps):
+                    A[i,j] = ((-(j+1)*dt)**i)/math.factorial(i)
+            b = np.zeros(self.steps)
+            b[0] = 1
+            lu, piv = lu_factor(A)
+            
+            # bb is the coefficients with b1 at the top
+            bb = lu_solve((lu, piv), b)
+            
+            LHS = self.M*a[0] + self.L
+            
+            First_RHS_Term = bb[0]*self.FX_archives[-1]
+            for i in range(1, self.steps):
+                First_RHS_Term += bb[i]*self.FX_archives[-(i+1)]
+                
+            Second_RHS_Term = a[1]*self.X_archives[-1]
+            for i in range(2, self.steps+1):
+                Second_RHS_Term += a[i]*self.X_archives[-i]
+                
+            RHS = First_RHS_Term - (self.M @ Second_RHS_Term)
+            
+            self.LU = spla.splu(LHS.tocsc(), permc_spec='NATURAL')
+            new_X = self.LU.solve(RHS)
+            return new_X
+            
